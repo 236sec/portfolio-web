3b2bbb52ba280730f2205fe359cc4cd306824cfd
236sec <popole159za@gmail.com>
2026-03-09 19:13:34 +0700

    setup deploy nextjs to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  reactStrictMode: true,
+  assetPrefix: isProd ? "/portfolio-web/" : "",
 };
 
 export default nextConfig;
