f344a66e7c5e9b1ce5cf18523ba8ee1d79db36c8
236sec <popole159za@gmail.com>
2026-03-09 20:02:55 +0700

    Second Try fix Image

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const basePath = "/portfolio-web";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/portfolio-web",
-  assetPrefix: "/portfolio-web/",
+  basePath,
+  assetPrefix: `${basePath}/`,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
   reactStrictMode: true,
   images: {
     unoptimized: true, // Optional: Use this if you're hosting images statically
