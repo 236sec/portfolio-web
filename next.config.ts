abd8987901dea6f42f1410bc1be1b39c66c93b19
236sec <popole159za@gmail.com>
2026-05-05 02:13:50 +0700

    Fix Image Path Ver.1

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: true,
   images: {
+    path: `${basePath}/_next/image`,
     unoptimized: true, // Optional: Use this if you're hosting images statically
     remotePatterns: [
       {
